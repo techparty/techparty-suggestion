5e6b61ec28f78ace95a5469c4713d55b6b7acc1f
Fernando Porazzi <fporazzi46@gmail.com>
2014-04-07 20:19:14 -0300

    Add options

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ module.exports = function (app) {
 
   routes.index = function (req, resp) {
     resp.render('index');
-  }
+  };
+
+  routes.submit = function (req, resp) {
+  	req.send();
+  };
 
   app.get('/', routes.index);
+  app.post('/submit', routes.submit);
 
 };
