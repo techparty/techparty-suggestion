707e8207eea0b0574e5d5972b4367d9f7de4b823
Daniel Röhers Moura <rohersmoura@gmail.com>
2014-04-08 16:34:18 -0300

    layout, rating
    - implemented layout for home and rating
    - implemented rating

--- helper/useful.js
+++ helper/useful.js
@@ -2,12 +2,55 @@
 
 var useful = (function () {
 
-  var exports = {}
+  var exports = {},
+      _suggestionsDefault = [
+        { name: 'es6', value: 'JavaScript ES6' },
+        { name: 'es7', value: 'JavaScript ES7' },
+        { name: 'nodejs', value: 'NodeJs' },
+        { name: 'oocss', value: 'OOCSS' },
+        { name: 'css3', value: 'CSS 3' },
+        { name: 'css_bem', value: 'CSS BEM' },
+        { name: 'web_components', value: 'Web Components' },
+        { name: 'shadow_dom', value: 'Shadow DOM' },
+        { name: 'seo', value: 'SEO' },
+        { name: 'elastic_search', value: 'ElasticSearch' },
+        { name: 'java_8', value: 'Java 8' },
+        { name: 'reactjs', value: 'ReactJS' },
+        { name: 'css_preprocessors', value: 'CSS Preprocessors' },
+        { name: 'wai_aria', value: 'WAI-ARIA' },
+        { name: 'rwr', value: 'Responsive Web Design' },
+        { name: 'html5', value: 'HTML 5' },
+        { name: 'ios', value: 'Desenvolvimento para iOS' },
+        { name: 'andoid', value: 'Desenvolvimento para Android' },
+        { name: 'performance', value: 'Performance' },
+        { name: 'startup', value: 'Startups' },
+        { name: 'github', value: 'GitHub' },
+        { name: 'webgl', value: 'WebGL' },
+        { name: 'midias', value: 'Mídias Sociais' },
+        { name: 'jquery', value: 'jQuery' },
+        { name: 'api_html5', value: 'APIs de HTML5' },
+        { name: 'acessibilidade', value: 'Acessibilidade' },
+        { name: 'yeoman', value: 'Yeoman' },
+        { name: 'firefox', value: 'Firefox OS' },
+        { name: 'heroku', value: 'Heroku Cloud Computing' },
+        { name: 'meteor', value: 'Meteor' },
+        { name: 'javascript_template', value: 'JavaScript Templates' },
+        { name: 'ruby', value: 'Ruby' },
+        { name: 'xmlhttprequest', value: 'XMLHttpRequest Level 2'}
+      ].sort(_compareAsc);
 
-  var _compare = function (a, b) {
+  function _compareAsc (a, b) {
     if (a.value > b.value)
       return 1;
     if (a.value < b.value)
+      return -1;
+    return 0;
+  };
+
+  function _compareDesc (a, b) {
+    if (a.value < b.value)
+      return 1;
+    if (a.value > b.value)
       return -1;
     return 0;
   };
@@ -19,65 +62,53 @@ var useful = (function () {
         if (element === 'suggestion') {
           values.push(opt[element]);
         } else {
-          values.push(element);
+          _suggestionsDefault.map(function (el) {
+            if (el.name === element) {
+              values.push(el.value)
+            }
+          });
         }
       }
     });
     return values;
   };
 
-  exports.compare = function (a, b) {
-    return _compare(a, b);
+  var _forEach = function (array, callback) {
+    for (var i = 0; i < array.length; i++) {
+      callback(array[i]);
+    }
   };
+
+  var _uniqueArray = function (array) {
+    return array.filter(function(elem, pos) {
+      return array.indexOf(elem) == pos;
+    }); 
+  }
+
+  exports.compareAsc = function (a, b) {
+    return _compareAsc(a, b);
+  };
+
+  exports.compareDesc = function (a, b) {
+    return _compareDesc(a, b);
+  };  
 
   exports.getSuggestions = function (opt) {
     return _getSuggestions(opt);
   };
 
-  exports.suggestionsDefault = function () {
-    return [
-      { name: 'es6', value: 'JavaScript ES6' },
-      { name: 'es7', value: 'JavaScript ES7' },
-      { name: 'nodejs', value: 'NodeJs' },
-      { name: 'oocss', value: 'OOCSS' },
-      { name: 'css3', value: 'CSS 3' },
-      { name: 'css_bem', value: 'CSS BEM' },
-      { name: 'web_components', value: 'Web Components' },
-      { name: 'shadow_dom', value: 'Shadow DOM' },
-      { name: 'seo', value: 'SEO' },
-      { name: 'elastic_search', value: 'ElasticSearch' },
-      { name: 'java_8', value: 'Java 8' },
-      { name: 'reactjs', value: 'ReactJS' },
-      { name: 'css_preprocessors', value: 'CSS Preprocessors' },
-      { name: 'wai_aria', value: 'WAI-ARIA' },
-      { name: 'rwr', value: 'Responsive Web Design' },
-      { name: 'html5', value: 'HTML 5' },
-      { name: 'ios', value: 'Desenvolvimento para iOS' },
-      { name: 'andoid', value: 'Desenvolvimento para Android' },
-      { name: 'performance', value: 'Performance' },
-      { name: 'startup', value: 'Startups' },
-      { name: 'github', value: 'GitHub' },
-      { name: 'webgl', value: 'WebGL' },
-      { name: 'midias', value: 'Mídias Sociais' },
-      { name: 'jquery', value: 'jQuery' },
-      { name: 'api_html5', value: 'APIs de HTML5' },
-      { name: 'acessibilidade', value: 'Acessibilidade' },
-      { name: 'yeoman', value: 'Yeoman' },
-      { name: 'firefox', value: 'Firefox OS' },
-      { name: 'heroku', value: 'Heroku Cloud Computing' },
-      { name: 'meteor', value: 'Meteor' },
-      { name: 'javascript_template', value: 'JavaScript Templates' },
-      { name: 'ruby', value: 'Ruby' },
-      { name: 'xmlhttprequest', value: 'XMLHttpRequest Level 2'}
-    ].sort(_compare);
+  exports.suggestionsDefault = _suggestionsDefault;
+
+  exports.techparty = 'TechParty';
+
+  exports.year = 2015;
+
+  exports.forEach = function (array, callback) {
+    _forEach(array, callback);
   }
 
-  exports.techparty = function () {
-    return 'TechParty';
-  }
-
-  exports.year = function () {
-    return 2015;
+  exports.uniqueArray = function (array) {
+    return _uniqueArray(array);
   }
 
   return exports;
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ module.exports = function (app) {
 
   var useful = require('../helper/useful'),
       Suggestion = require('../models/suggestion'),
+      techparty = useful.techparty,
+      year = useful.year,
       routes = {};
 
-  routes.index = function (req, res) {
-    res.render('index', {
+  routes.home = function (req, res) {
+    res.render('home', {
       suggestions: useful.suggestionsDefault,
-      techparty: useful.techparty,
-      year: useful.year
+      techparty: techparty,
+      year: year
     });
   };
 
@@ -44,10 +46,40 @@ module.exports = function (app) {
   };
 
   routes.rating = function (req, res) {
-    res.render('rating');
+    var suggestions = Suggestion.find(function (err, suggestions) {
+      if (err) return console.error(err);
+
+      var unique = [];
+      useful.forEach(suggestions, function (el) {
+        useful.forEach(el.suggestion, function (value) {
+          if (value) {
+            unique.push(value);
+          }
+        });
+      });
+
+      var result = [];
+      useful.forEach(useful.uniqueArray(unique), function (value) {
+        var count = 0;
+        useful.forEach(suggestions, function (el) {
+          useful.forEach(el.suggestion, function (suggestion) {
+            if (suggestion == value) {
+              count += 1;
+            }
+          });
+        });
+        result.push({name: value, value: count});
+      });
+
+      res.render('rating', {
+        suggestions: result.sort(useful.compareDesc),
+        techparty: techparty,
+        year: year
+      })
+    })
   }
 
-  app.get('/', routes.index);
+  app.get('/', routes.home);
   app.post('/submit', routes.submit);
   app.get('/rating', routes.rating);
 
