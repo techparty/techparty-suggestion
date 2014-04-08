9bba713017ea2b3bce7472b6bc3cd43d693c3305
Daniel Röhers Moura <rohersmoura@gmail.com>
2014-04-07 21:17:37 -0300

    suggestions columns, added suggestions
    - added more suggestions
    - separate columns in the suggestions

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,48 @@ module.exports = function (app) {
 
   'use strict';
 
-  var routes = {};
-  var suggestions = [
-      {name: 'es6', value: 'JavaScript ES6'},
-      {name: 'es7', value: 'JavaScript ES7'},
-      {name: 'nodejs', value: 'NodeJs'},
-      {name: 'oocss', value: 'OOCSS'},
-      {name: 'css3', value: 'CSS3'},
-      {name: 'css-bem', value: 'CSS BEM'},
-      {name: 'web-components', value: 'Web Components'},
-      {name: 'shadow-dom', value: 'Shadow DOM'},
-      {name: 'seo', value: 'SEO'},
-      {name: 'elastic-search', value: 'ElasticSearch'},
-      {name: 'java-8', value: 'Java 8'},
-      {name: 'reactjs', value: 'ReactJS'},
-      {name: 'css-preprocessors', value: 'CSS Preprocessors'},
-      {name: 'wai-aria', value: 'WAI-ARIA'},
-      {name: 'rwr', value: 'Responsive Web Design'},
-      {name: 'html5', value: 'HTML 5'},
-      {name: 'ios', value: 'Desenvolvimento para iOS'},
-      {name: 'andoid', value: 'Desenvolvimento para Android'},
-      {name: 'performance', value: 'Performance'},
-      {name: 'startup', value: 'Startups'},
-      {name: 'github', value: 'Github'},
-      {name: 'webgl', value: 'WebGL'},
-      {name: 'midias', value: 'Mídias Sociais'},
-      {name: 'jquery', value: 'jQuery'},
-      {name: 'api-html5', value: 'APIs de HTML5'}
-    ].sort();
+  var routes = {},
+      year = 2015,
+      techparty = 'TechParty',
+      suggestions = [
+        { name: 'es6', value: 'JavaScript ES6' },
+        { name: 'es7', value: 'JavaScript ES7' },
+        { name: 'nodejs', value: 'NodeJs' },
+        { name: 'oocss', value: 'OOCSS' },
+        { name: 'css3', value: 'CSS3' },
+        { name: 'css-bem', value: 'CSS BEM' },
+        { name: 'web-components', value: 'Web Components' },
+        { name: 'shadow-dom', value: 'Shadow DOM' },
+        { name: 'seo', value: 'SEO' },
+        { name: 'elastic-search', value: 'ElasticSearch' },
+        { name: 'java-8', value: 'Java 8' },
+        { name: 'reactjs', value: 'ReactJS' },
+        { name: 'css-preprocessors', value: 'CSS Preprocessors' },
+        { name: 'wai-aria', value: 'WAI-ARIA' },
+        { name: 'rwr', value: 'Responsive Web Design' },
+        { name: 'html5', value: 'HTML 5' },
+        { name: 'ios', value: 'Desenvolvimento para iOS' },
+        { name: 'andoid', value: 'Desenvolvimento para Android' },
+        { name: 'performance', value: 'Performance' },
+        { name: 'startup', value: 'Startups' },
+        { name: 'github', value: 'Github' },
+        { name: 'webgl', value: 'WebGL' },
+        { name: 'midias', value: 'Mídias Sociais' },
+        { name: 'jquery', value: 'jQuery' },
+        { name: 'api-html5', value: 'APIs de HTML5' },
+        { name: 'acessibilidade', value: 'Acessibilidade' },
+        { name: 'yeoman', value: 'Yeoman' },
+        { name: 'firefox', value: 'Firefox OS' },
+        { name: 'heroku', value: 'Heroku Cloud Computing' },
+        { name: 'meteor', value: 'Meteor' },
+        { name: 'javascript-template', value: 'JavaScript Templates' }
+      ].sort();
 
   routes.index = function (req, resp) {
     resp.render('index', {
-      suggestions: suggestions
+      suggestions: suggestions,
+      techparty: techparty,
+      year: year
     });
   };
 
