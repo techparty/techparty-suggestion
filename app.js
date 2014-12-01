a01a0c2548ea88c93e03092c259ae72ba17418aa
Daniel Röhers Moura <rohersmoura@gmail.com>
2014-12-01 15:21:55 -0200

    removed nodetime

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 'use strict';
 
-var newrelic = require('newrelic'),
-    flash = require('express-flash'),
-    express = require('express'),
-    app = express(),
-    hbs = require('hbs'),
-    path = require('path'),
-    fs = require('fs'),
-    mongoose = require ("mongoose"),
-    mail = require("nodemailer").mail;
-
-// config notetime
-// require('./nodetime');
+var newrelic = require('newrelic');
+var flash = require('express-flash');
+var express = require('express');
+var app = express();
+var hbs = require('hbs');
+var path = require('path');
+var fs = require('fs');
+var mongoose = require ("mongoose");
+var mail = require("nodemailer").mail;
 
 app.use(express.static('assets'));
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
-//app.use(express.bodyParser());
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -29,24 +25,22 @@ app.use(flash());
 var db = require('./config/db');
 
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-
-  //config = yaml_config.load(__dirname + 'config/config.yml');
+	app.use(express.errorHandler());
 }
 
 // models
 var modelsPath = path.join(__dirname, 'models');
 fs.readdirSync(modelsPath).forEach(function (file) {
-  require(modelsPath + '/' + file);
+	require(modelsPath + '/' + file);
 });
 
 // routes
 var routesPath = path.join(__dirname, 'routes');
 fs.readdirSync(routesPath).forEach(function(file) {
-  require(routesPath + '/' + file)(app);
+	require(routesPath + '/' + file)(app);
 });
 
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function () {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
--- nodetime.js
+++
@@ -1,8 +0,0 @@
-var accountKey = process.env.NODETIME;
-
-if (accountKey) {
-  nodetime.profile({
-    accountKey: process.env.NODETIME,
-    appName: 'techpartysuggestion'
-  });
-}
