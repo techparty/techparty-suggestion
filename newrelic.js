6b561f0736029055a44de2b5b32e073cbcf155dc
Daniel Röhers Moura <rohersmoura@gmail.com>
2014-12-01 16:13:37 -0200

    newrelic logging level

--- newrelic.js
+++ newrelic.js
@@ -5,23 +5,23 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name : ['techpartysuggestion'],
-  /**
-   * Your New Relic license key.
-   */
-  license_key : process.env.NEW_RELIC_KEY || 'license_key',
-  logging : {
-    /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
-    level : 'trace'
-  },
-  browser_monitoring : {
-    enable : false
-  }
+	/**
+	 * Array of application names.
+	 */
+	app_name : ['techpartysuggestion'],
+	/**
+	 * Your New Relic license key.
+	 */
+	license_key : process.env.NEW_RELIC_KEY || 'license_key',
+	logging : {
+		/**
+		 * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+		 * issues with the agent, 'info' and higher will impose the least overhead on
+		 * production applications.
+		 */
+		level : 'warn'
+	},
+	browser_monitoring : {
+		enable : false
+	}
 };
